d9902c3e1ee9c7b5499ed8991bac5378f0da19e0
KatheelinQuina <kthlnquina@gmail.com>
2025-10-26 16:41:31 +0700

    Membuat class Kontainer dan inisialisasi tarif (Katheelin)

---
+++ KB-BMK.py
@@ -0,0 +1,14 @@
+# Katheelin Quina
+
+# Program: Kalkulator Biaya Bongkar Muat Kontainer (KB-BMK)
+
+class Kontainer:
+    def __init__(self, jenis):
+        self.jenis = jenis
+        if self.jenis == "20":
+            self.tarif = 500000
+        elif self.jenis == "40":
+            self.tarif = 900000
+        else:
+            self.tarif = 0
+
