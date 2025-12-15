0f76ca6acf9e7a5a06bcf8159d0ffc370b01d860
kthlnquina <kthlnquina@gmail.com>
2025-12-15 11:04:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ def main():
     input_filename = input("Masukkan nama file CSV input (contoh: daftar_kapal.csv): ")
 
     print("\nTarif 20 feet/kontainer : Rp 500,000")
-    print("Tarif 20 feet/kontainer : Rp 900,000")
+    print("Tarif 40 feet/kontainer : Rp 900,000")
 
     try:
         with open(input_filename, mode='r', encoding='utf-8') as file:
